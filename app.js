cf8d7e1af07d359acf25af2b0101917cfe76146a
Shreyas Sreedhar <70530523+shreyas-sreedhar@users.noreply.github.com>
2020-12-03 16:29:43 +0530

    Learning Js-1

---
+++ app.js
@@ -0,0 +1,69 @@
+// variables for setup
+
+let container;
+let camera;
+let renderer;
+let scene;
+
+let house;
+function init(){
+    container = document.querySelector(".scene");
+
+    //scene buildmadbeku
+    scene = new THREE.Scene();
+
+    const fov = 35;
+    const aspect = container.clientWidth / container.clientHeight;
+    const near = 0.1;
+    const far = 1000;
+
+    //camera madu
+
+    camera = new THREE.PerspectiveCamera(fov,aspect,near,far);
+
+    camera.position.set(0,3,25);
+
+    //lights
+
+    const ambient = new THREE.AmbientLight(0x404040, 2);
+    scene.add(ambient);
+
+    const light = new THREE.DirectionalLight(0xffffff,2);
+    light.position.set(10,10,10);
+    scene.add(light);
+    //rendermadu
+
+    renderer = new THREE.WebGLRenderer({antialias: true, alpha: true});
+    renderer.setSize(container.clientWidth, container.clientHeight);
+    renderer.setPixelRatio(window.devicePixelRatio);
+
+    container.appendChild(renderer.domElement);
+
+    //model loadmadu
+    let loader = new THREE.GLTFLoader();
+    loader.load("./3d/scene.gltf", function(gltf){
+        scene.add(gltf.scene);
+        house =gltf.scene.children[0];
+        animate();
+
+    });
+
+}
+function animate(){
+    requestAnimationFrame(animate);
+    house.rotation.z += 0.005;
+    renderer.render(scene, camera);
+}
+
+init();
+
+
+function onWindowResize(){
+    camera.aspect = container.clientWidth / container.clientHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(container.clientWidth, container.clientHeight);
+
+
+}
+
+window.addEventListener("resize", onWindowResize);
